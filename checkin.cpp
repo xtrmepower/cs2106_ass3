702a743378d6822397048174fee66a4efa17a99f
Jay Chua <jaychua@hotmail.sg>
2016-04-19 02:54:27 +0800

    Added writeFile function.

--- checkin.cpp
+++ checkin.cpp
@@ -7,5 +7,24 @@ int main(int ac, char **av) {
 		return -1;
 	}
 
+	initFS("part.dsk", av[2]);
+
+	char *buffer = makeDataBuffer();
+	FILE *inFPtr = fopen(av[1], "r");
+	TFileSystemStruct *fs = getFSInfo();
+
+	//Open the current file to read.
+	fread(buffer, sizeof(char), fs->blockSize, inFPtr);
+
+	//Write into partition
+	int fp=openFile(av[1],MODE_CREATE);
+	writeFile(fp, buffer,0,0);
+
+	// Close the file
+	fclose(inFPtr);
+	free(buffer);
+
+	closeFS();
+
 	return 0;
 }
